933907a1b5647b65294990b64a295f71c846ffc1
jackie <ss611611@gmail.com>
2024-07-11 12:37:24 +0800

    Initial commit

---
+++ Sample Admob/Sample_AdmobApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sample_AdmobApp.swift
+//  Sample Admob
+//
+//  Created by Jackie Lu on 2024/7/11.
+//
+
+import SwiftUI
+
+@main
+struct Sample_AdmobApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
